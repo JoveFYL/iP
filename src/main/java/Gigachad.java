9f7c9434c6924ad5f302e56f3d1152b025a73800
Jove <jovefong62@gmail.com>
2025-08-20 21:42:02 +0800

    Refactor Gigachad to echo commands entered by the user

--- src/main/java/Gigachad.java
+++ src/main/java/Gigachad.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Gigachad {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,5 +10,16 @@ public class Gigachad {
         System.out.println("Hello! I'm Gigachad");
         System.out.println("What can I do for you?");
         System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a command: ");
+        String command = "";
+        while (!command.equals("bye")) {
+            command = scanner.nextLine();
+            System.out.println(command);
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
+        scanner.close();
     }
 }
