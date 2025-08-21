46128ff113739198cf7d37c29ff1e3d5bd4411fc
Jove <jovefong62@gmail.com>
2025-08-21 17:38:04 +0800

    Implement ToDos, Events, Deadlines

--- src/main/java/Gigachad.java
+++ src/main/java/Gigachad.java
@@ -24,39 +24,79 @@ public class Gigachad {
 
         while (!command.equals("bye")) {
             command = scanner.nextLine();
+            String[] parts = command.split(" ");
+            String firstWord = parts[0].toLowerCase();
 
-            if (command.equals("list")) {
-                for (int i = 0; i < listOfTasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + listOfTasks.get(i));
-                }
-            } else if (command.startsWith("mark")) {
-                String[] parts = command.split(" ");
-                if (parts.length == 2) {
-                    int taskNumber = Integer.parseInt(parts[1]) - 1;
-                    if (taskNumber > listOfTasks.size()) {
-                        System.out.println("Invalid task!");
-                    } else {
-                        listOfTasks.get(taskNumber).markAsDone();
+            switch (firstWord) {
+                case "list":
+                    for (int i = 0; i < listOfTasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + listOfTasks.get(i));
                     }
-                }
-            } else if (command.startsWith("unmark")) {
-                String[] parts = command.split(" ");
-                if (parts.length == 2) {
-                    int taskNumber = Integer.parseInt(parts[1]) - 1;
-                    if (taskNumber > listOfTasks.size()) {
-                        System.out.println("Invalid task!");
-                    } else {
-                        listOfTasks.get(taskNumber).unmark();
+                    break;
+                case "mark":
+                    if (parts.length == 2) {
+                        int taskNumber = Integer.parseInt(parts[1]) - 1;
+                        if (taskNumber > listOfTasks.size()) {
+                            System.out.println("Invalid task!");
+                        } else {
+                            listOfTasks.get(taskNumber).markAsDone();
+                        }
                     }
-                }
-            } else {
-                Task newTask = new Task(command);
-                listOfTasks.add(newTask);
-                System.out.println("added: " + newTask.getDescription());
+                    break;
+                case "unmark":
+                    if (parts.length == 2) {
+                        int taskNumber = Integer.parseInt(parts[1]) - 1;
+                        if (taskNumber > listOfTasks.size()) {
+                            System.out.println("Invalid task!");
+                        } else {
+                            listOfTasks.get(taskNumber).unmark();
+                        }
+                    }
+                    break;
+                case "todo":
+                    String todoDescription = command.substring(5); // skip "todo "
+                    ToDo todo = new ToDo(todoDescription);
+                    listOfTasks.add(todo);
+
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(todo);
+                    System.out.println("Now you have " + listOfTasks.size() + " tasks in the list.");
+                    break;
+                case "deadline":
+                    int firstDeadlineSpace = command.indexOf(' ');
+                    int byIndexDeadline = command.indexOf('/');
+
+                    String deadlineDescription = command.substring(firstDeadlineSpace + 1, byIndexDeadline).trim();
+                    String deadlineDueDate = command.substring(byIndexDeadline + 4).trim();
+
+                    Deadline deadline = new Deadline(deadlineDescription, deadlineDueDate);
+                    listOfTasks.add(deadline);
+
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(deadline);
+                    System.out.println("Now you have " + listOfTasks.size() + " tasks in the list.");
+                    break;
+                case "event":
+                    int firstSpace = command.indexOf(' ');
+                    int fromIndexEvent = command.indexOf(" /from");
+                    int toIndexEvent = command.indexOf(" /to");
+
+                    String eventDescription = command.substring(firstSpace + 1, fromIndexEvent).trim();
+                    String from = command.substring(fromIndexEvent + 7, toIndexEvent).trim();
+                    String to = command.substring(toIndexEvent + 5).trim();
+
+                    Event event = new Event(eventDescription, from, to);
+                    listOfTasks.add(event);
+
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(event);
+                    System.out.println("Now you have " + listOfTasks.size() + " tasks in the list.");
+                    break;
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    scanner.close();
+                    break;
             }
         }
-
-        System.out.println("Bye. Hope to see you again soon!");
-        scanner.close();
     }
 }
