9d969519ed5d1bf19bf87b0008e855a024d79a1e
Jove <jovefong62@gmail.com>
2025-08-21 11:58:39 +0800

    Add missing Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
-
 public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void markAsDone() {
+        if (!isDone) {
+            isDone = true;
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println(this);
+        } else {
+            System.out.println("I already marked this task as done!");
+        }
+    }
+
+    public void unmark() {
+        if (isDone) {
+            isDone = false;
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println(this);
+        } else {
+            System.out.println("I already marked this task as undone!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
 }
