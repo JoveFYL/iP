ebd0a30689f97f3e1a106e83e85d0ecea4a0274c
Jove <jovefong62@gmail.com>
2025-09-13 10:47:46 +0800

    Refactor Command.java to align to Code Quality guidelines

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,7 +13,7 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/Gigachad.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/gigachad/Gigachad.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
